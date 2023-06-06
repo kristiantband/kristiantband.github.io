49572c8de32bc5ed92fe93478d7784091ae4da3e
kristiantband <kristiantband@outlook.com>
2023-06-06 01:29:50 +0100

    Flex cards created and updated CSS

---
+++ js/ktb_back_to_top_script.js
@@ -0,0 +1,19 @@
+// Get the button
+let mybutton = document.getElementById("btn-gtt");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
